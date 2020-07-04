a401805d9a5820e50a2216d55597c4123ee9bfdc
pradeepmg <mgpradeepa@gmail.com>
2020-07-05 01:54:52 +0530

    updated build scripts

--- src/main/scala/com/mgp/springbootscala/repo/CompanyRepo.scala
+++ src/main/scala/com/mgp/springbootscala/repo/CompanyRepo.scala
@@ -2,6 +2,7 @@ package com.mgp.springbootscala.repo
 
 import com.mgp.springbootscala.domain.Company
 import org.springframework.data.repository.CrudRepository
+import java.lang.Long
 
 trait CompanyRepo  extends CrudRepository[Company, Long]{
 }
